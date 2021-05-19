8117aebacbbbd0722703ff7a98404b06a1fc01fa
gilgardosh <gilgardosh@gmail.com>
2021-05-19 18:06:42 +0300

    updated resolvers

--- src/resolvers/importTransactionsToBatchResolver.js
+++ src/resolvers/importTransactionsToBatchResolver.js
@@ -2,47 +2,47 @@ module.exports = (next) => (root, args, context, info) => {
   if (!args.input) {
     args.input = {};
   }
-  const rows = args.input.transactionsList.map((t) => {
+  const rows = args.input.rows.map((t) => {
     let moves = [];
-    if (t.records) {
-      moves = t.records.map((r) => ({
-        AccountKey: r.accountId,
-        DebitCredit: r.debitOrCreditNumber,
-        SuF: r.shekelSum,
-        SuFDlr: r.foreignCurrencySum,
+    if (t.moves) {
+      moves = t.moves.map((r) => ({
+        AccountKey: r.AccountKey,
+        DebitCredit: r.DebitCredit,
+        SuF: r.SuF,
+        SuFDlr: r.SuFDlr,
       }));
     }
     return {
-      Branch: t.branch,
-      CostCode: t.costingCode,
-      CredName: t.creditorName,
-      CurrencyCode: t.currencyCode,
-      DatF3: t.date3,
-      DebName: t.debtorName,
-      Description: t.description,
-      Det2: t.details2,
-      Details: t.details1,
-      DueDate: t.dueDate,
-      Osek874: t.authorizedDealerNumber,
-      Quant: t.quantity,
-      Ref2: t.reference2,
-      Ref3: t.reference3,
-      Referance: t.reference1,
-      SuF: t.shekelSum,
-      SuFDlr: t.foreignCurrencySum,
-      TransCredID: t.creditorId,
-      TransDebID: t.debtorId,
-      TransType: t.type,
-      ValueDate: t.valueDate,
+      Branch: t.Branch,
+      CostCode: t.CostCode,
+      CredName: t.CredName,
+      CurrencyCode: t.CurrencyCode,
+      DatF3: t.DatF3,
+      DebName: t.DebName,
+      Description: t.Description,
+      Det2: t.Det2,
+      Details: t.Details,
+      DueDate: t.DueDate,
+      Osek874: t.Osek874,
+      Quant: t.Quant,
+      Ref2: t.Ref2,
+      Ref3: t.Ref3,
+      Referance: t.Referance,
+      SuF: t.SuF,
+      SuFDlr: t.SuFDlr,
+      TransCredID: t.TransCredID,
+      TransDebID: t.TransDebID,
+      TransType: t.TransType,
+      ValueDate: t.ValueDate,
       moves,
     };
   });
 
   args.input = {
-    batchNo: args.input.batchId,
-    insertolastb: args.input.insertToLastBatch,
-    check: args.input.checkBatch,
-    issue: args.input.issueBatch,
+    batchNo: args.input.batchNo,
+    insertolastb: args.input.insertolastb,
+    check: args.input.check,
+    issue: args.input.issue,
     rows,
   };
   return next(root, args, context, info);
